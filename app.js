1fc986a216d942166ed5bc2a0d2c16f01eef7eb8
Jeremy Cantu <jac21@tamu.edu>
2015-03-26 08:45:14 -0500

    Test commit with Alchemy API

--- app.js
+++ app.js
@@ -39,3 +39,47 @@ var port = (process.env.VCAP_APP_PORT || 3000);
 app.listen(port, host);
 console.log('App started on port ' + port);
 
+
+
+//TODO: BEGIN RESTful API
+
+var demo_text = 'Yesterday dumb Bob destroyed my fancy iPhone in beautiful Denver, Colorado. I guess I will have to head over to the Apple Store and buy a new one.';
+var demo_url = 'http://www.npr.org/2013/11/26/247336038/dont-stuff-the-turkey-and-other-tips-from-americas-test-kitchen';
+var demo_html = '<html><head><title>Node.js Demo | AlchemyAPI</title></head><body><h1>Did you know that AlchemyAPI works on HTML?</h1><p>Well, you do now.</p></body></html>';
+
+function example(req, res) {
+	var output = {};
+	
+	//start analyzing
+	entities(req, res, output);
+}
+
+function entities(req, res, output) {
+	alchemyapi.entities('text', demo_text, {'sentiment':1}, function(response) {
+		output['entities'] = {text:demo_text, response:JSON.stringify(response, null, 4), results:response['entities'] }
+	})
+}
+
+function keywords(req, res, output) {
+	alchemyapi.keywords('text', demo_text, { 'sentiment':1 }, function(response) {
+		output['keywords'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['keywords'] };
+		concepts(req, res, output);
+	});
+}
+
+
+function concepts(req, res, output) {
+	alchemyapi.concepts('text', demo_text, { 'showSourceText':1 }, function(response) {
+		output['concepts'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['concepts'] };
+		sentiment(req, res, output);
+	});
+}
+
+
+function sentiment(req, res, output) {
+	alchemyapi.sentiment('html', demo_html, {}, function(response) {
+		output['sentiment'] = { html:demo_html, response:JSON.stringify(response,null,4), results:response['docSentiment'] };
+		text(req, res, output);
+	});
+}
+
